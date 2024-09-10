964883501cd82069e1c313a289ea4deaf68c0f69
Shivani Dubey <108396731+CoderShivi@users.noreply.github.com>
2024-09-10 13:40:43 +0000

    datatypes

---
+++ basicsJs/01_variables.js
@@ -0,0 +1,7 @@
+const accountId=1234
+let accountEmail="shiv@gmail.com"
+var accountName="Shivani Dubey"
+accountPassword=6666
+let accountNo
+console.table([accountId,accountEmail,accountName,accountPassword,accountNo])
+/* Prefer Not use of var because of issue of block scope and functional scope */
---
+++ basicsJs/02_datatypes.js
@@ -0,0 +1,10 @@
+let number=12
+//Number
+//bigint
+//String ""
+//Boolean => true/false
+//undefined
+//Symbol=> Unique
+//Object
+console.log(typeof null)  // Object
+console.log(typeof undefined)
