a56ae9733ce6242d066e65190bc2fe11bb56354f
Shivani Dubey <108396731+CoderShivi@users.noreply.github.com>
2024-09-11 12:42:19 +0000

    Objects

--- basicsJs/objects.js
+++ basicsJs/objects.js
@@ -33,6 +33,44 @@ user.greeting=function(){
 user.greeting_1=function(){
     console.log(`Hello ${this.name}`)
 }
+// console.log(user.greeting())
+// console.log(user.greeting_1())
 
-console.log(user.greeting())
-console.log(user.greeting_1())
+// How we decalre object through contructor
+const user_1=new Object() // It is known as Singleton Object
+const user_2={}// it is known as non-singleton Object
+user_1.name="Mahi"
+user_1.age=22
+//console.log(user_1)
+
+const regularUser={
+    email:'mahi223@gmail.com',
+    fullName:{
+        userFullName:{
+            firstName:'shiva',
+            lastName:'Dubey'
+        }
+    }
+}
+//console.log(regularUser.fullName.userFullName.lastName)
+
+const obj={1:'abs',2:'cde'}
+const obj1={4:'lkl',5:'nnc'}
+//const obj2=Object.assign({}, obj, obj1) //{} it is a target object
+const obj2={...obj,...obj1}
+// console.log(obj2)
+// console.log(Object.keys(obj2))
+// console.log(Object.values(obj2))
+// console.log(Object.entries(obj2))
+// console.log(obj2.hasOwnProperty(1))
+
+// Destructuring
+const course={
+    courseName:'javaScript',
+    price:999,
+    courseinstructor:'Hitesh'
+}
+const {courseinstructor}=course
+const {courseinstructor:instructor}=course
+console.log(courseinstructor)
+console.log(instructor)
