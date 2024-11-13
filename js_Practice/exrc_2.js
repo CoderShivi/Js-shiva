6326f9d1a78d030f605c4126d5a2f0d34dd10260
Shivani Dubey <108396731+CoderShivi@users.noreply.github.com>
2024-11-13 12:14:04 +0000

    Exercise_2

--- js_Practice/exrc_2.js
+++ js_Practice/exrc_2.js
@@ -23,3 +23,35 @@ function halfOfString(str){
  }
 }
 halfOfString("Shiv")
+
+//Write a javaScript program to concatenate two strings except their first character.
+function concateString(str1,str2){
+   str3=str1.slice(1)+str2.slice(1)
+   console.log(str3)
+}
+concateString("sshiv","ddubey")
+
+// Given two values, write a javaScript program to find out which one is nearest to 100
+function nearestNoTo100(a,b){
+   const diffA=Math.abs(100-a) 
+   const diffB=Math.abs(100-b)
+   if(diffA<diffB){
+      return a
+   }
+   else if (diffA>diffB){
+      return b
+   }
+   else{
+      console.log("Both values are equally close to 100")
+   }
+}
+
+console.log(nearestNoTo100(98,97))
+
+// Write a JavaScript program to check a given string contains 2 to 4 occurrences of a specified character.
+function countChars(str,char){
+   const count= str.split(char).length-1
+   return count >= 2 && count <= 4;
+
+}
+console.log(countChars("helllo","l"))
